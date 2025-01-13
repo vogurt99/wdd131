e84f60c838a14797c76f2866843623137cb5865c
vogurt99 <vogurtsjrg@gmail.com>
2025-01-13 10:46:50 +0800

    Week 2 Learning Activities 1-4.

---
+++ week02/scripts/bom.js
@@ -0,0 +1,29 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+button.addEventListener('click', function() {
+    if (input.value.trim() !== '') {
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
+
+        li.append(deleteButton);
+        list.append(li);
+        
+        input.value = '';
+        input.focus();
+
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li);
+            input.focus();
+        });
+    }
+    else {
+        alert('Input cannot be empty! Please enter a chapter.');
+        input.focus();
+        return;
+    }
+});
